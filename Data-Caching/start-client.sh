40ef4c605d10309e4db2c7683ea5972c220bdf09
Jeongseob Ahn <ahnjeong@umich.edu>
2015-03-15 20:20:21 -0400

    Slightly modified Data-Caching/start-client.sh to handle rps

--- Data-Caching/start-client.sh
+++ Data-Caching/start-client.sh
@@ -2,6 +2,12 @@
 ## File: Data-Caching/start-client.sh
 ## Usage: start-client.sh [RPS] [file path]
 ## Author: Yunqi Zhang (yunqi@umich.edu)
+## Modified by Jeongseob Ahn
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 [RPS] [file path]"
+	exit 1
+fi
 
 BENCHMARK="Data-Caching"
 
@@ -17,6 +23,15 @@ echo "[$BENCHMARK] The machine has $NUM_CORE logical cores in total."
 
 # Run the test
 echo "[$BENCHMARK] Run the test with 80% GET forever and $NUM_CORE threads."
-unbuffer numactl -C 6-11,18-23 -m 1 \
-  ./loader -a ../twitter_dataset/twitter_dataset_30x \
-  -s servers.txt -g 0.8 -T 10 -c 256 -w 12 -r "$RPS" > "$OUTPUT"
+# Configure memcached loader
+
+# NOTE: it does not consider NUMA affinity
+cmd="unbuffer ./loader -a ../twitter_dataset/twitter_dataset_30x \
+	  -s servers.txt -g 0.8 -T 10 -c 256 -w $NUM_CORE"
+
+# Check rps parameter
+if [ $RPS != '0' ]; then
+	cmd="$cmd -r $RPS"
+fi
+
+$cmd > "$OUTPUT"
--- README.md
+++ README.md
@@ -2,3 +2,7 @@ ClearSuite
 ==========
 
 A set of bash scripts to setup CloudSuite Benchmarks.
+
+Author: Yunqi Zhang
+
+Modified by Jeongseob Ahn
